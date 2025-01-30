f04252c8baa789d86cc3bc47f73d28afcfa8e556
Abhijat sinha <163438077+Abhijat05@users.noreply.github.com>
2025-01-30 19:31:25 +0530

    Update Group.java

--- src/Group.java
+++ src/Group.java
@@ -1,75 +1,39 @@
 import java.io.Serializable;
-import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
 
 import javax.swing.JOptionPane;
 
-public class Group implements Serializable{
-	private static final long serialVersionUID = 1L;
-	
-	private String name;
-	private String description;
-	private ArrayList<String> ListOfMembersInGroup = new ArrayList<>();
-	private boolean flag;
-	private boolean flag1;
-	private int number;
-	private int i;
-	
-	public Group(String name, String description)
-	{
-		this.name = name;
-		this.description = description;
-	}
-	
-	public boolean IsInGroup(User user)
-	{
-		//If group is empty
-		if(ListOfMembersInGroup.isEmpty())
-		{
-			flag1 = false;
-		}
-		else
-		{
-			flag = ListOfMembersInGroup.contains(user.getName());
-			if(flag == false)
-			{
-				flag1 = false; //If name is not in the list
-			}
-			else
-			{
-				flag1 = true; //If name is in the list
-			}
-		}
-		return flag1;
-	}
-	
-	public void EnrollInGroup(User user)
-	{
-		flag = IsInGroup(user); //See if user is already in the group or not
-		if(flag == false) //If they are not
-		{
-			ListOfMembersInGroup.add(user.getName()); //Add user in the group list
-			System.out.println(user.getName() +" has been successfully enrolled in group "+ name);
-			JOptionPane.showMessageDialog(null, user.getName() +" has been successfully enrolled in group "+ name);
-		}
-		else
-		{
-			JOptionPane.showMessageDialog(null, "You are already in this group");
-		}
-	}
+public class Group implements Serializable {
+    private static final long serialVersionUID = 1L;
+    
+    private String name;
+    private String description;
+    private Set<String> members = new HashSet<>();
+    
+    public Group(String name, String description) {
+        this.name = name;
+        this.description = description;
+    }
 
-	public int getNumber() {
-		return number;
-	}
+    public boolean isInGroup(User user) {
+        return members.contains(user.getName());
+    }
 
-	public int getI() {
-		return i;
-	}
+    public void enrollInGroup(User user) {
+        if (members.add(user.getName())) { // HashSet ensures uniqueness
+            System.out.println(user.getName() + " has been successfully enrolled in group " + name);
+            JOptionPane.showMessageDialog(null, user.getName() + " has been successfully enrolled in group " + name);
+        } else {
+            JOptionPane.showMessageDialog(null, "You are already in this group");
+        }
+    }
 
-	public String getDescription() {
-		return description;
-	}
+    public String getDescription() {
+        return description;
+    }
 
-	public String getName() {
-		return name;
-	}
+    public String getName() {
+        return name;
+    }
 }
